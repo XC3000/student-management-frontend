7cc627025a8959473e8b29377dc0ce2e36edfb96
Richard Henry Rozario <richardrozario.rr@gmail.com>
2022-09-03 01:27:02 +0530

    base url swaped

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5000"
-    : "https://testimonial-node.herokuapp.com";
+    : "https://student-backend-123.herokuapp.com";
 
 export const axiosInstance = axios.create({
   baseURL: `${BASE_URL}/api`,
